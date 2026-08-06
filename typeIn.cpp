d0c6dae6fad35352f82d339edfc0efc6f115f706
Jsong <lala5566z@163.com>
2026-08-06 18:22:37 +0800

    typeIn

--- typeIn.cpp
+++ typeIn.cpp
@@ -14,7 +14,7 @@ int saveToNew()
     ifile.close();
     std::ofstream file(name, std::ios::binary);
     for (auto &i : wordTable)
-        i.write(file);
+        file << i;
     return 0;
 }
 
@@ -26,11 +26,11 @@ int saveToExist()
     std::ofstream file(name, std::ios::app);
     if (!file.is_open())
     {
-        std::cout << "error\n";
-        return -1;
+        std::cout << "can not open file: " << name << "\n";
+        return 1;
     }
     for (auto &i : wordTable)
-        i.write(file);
+        file << i;
     return 0;
 }
 
@@ -41,16 +41,18 @@ int readFromFile()
 
     std::cin >> address;
     if (address == "/back")
-        return -1;
+        return 1;
 
     // 添加文件大小限制
     std::ifstream file(address);
     if (!file.is_open())
-        return 1;
-    std::string eng, chi;
-    while (!file.eof())
     {
-        file >> eng >> chi;
+        std::cout << "can not open file: " << address << std::endl;
+        return 1;
+    }
+    std::string eng, chi;
+    while (file >> eng >> chi)
+    {
         wordTable.emplace_back(eng, chi);
     }
     return 0;
@@ -68,20 +70,75 @@ int readFromKey()
         std::cin >> chi;
         wordTable.emplace_back(eng, chi);
     }
-    // debug
-    for (int i = 0; i < wordTable.size(); ++i)
-    {
-        std::cout << i << "\t";
-        std::cout << wordTable[i];
-    }
-    // debug
     return 0;
 }
 
 int main()
 {
-    readFromFile();
-    saveToNew();
+    /*
+    uint choose = 0;
+    std::vector<std::string> v(2);
+    std::string chosen = " -> ";
+    std::string nchosen = "    ";
+    while (true)
+    {
+        std::cout << "choose which way to type in:\n";
+        std::cout << "1.screen\n";
+        for (int i = 0; i < v.size(); ++i)
+        {
+            if (i == choose)
+                std::cout << chosen;
+            else
+                std::cout << nchosen;
+            std::cout << v[i];
+        }
 
+    }
+    */
+    int choose;
+    std::string cmd;
+loop:
+    std::cout << "choose which way to type in:\n";
+    std::cout << "1.From file\n";
+    std::cout << "2.From keyboard\n";
+    std::cin >> choose;
+    if (choose == 1)
+    {
+        if (readFromFile() == 1)
+            goto loop;
+    }
+    else if (choose == 2)
+    {
+        if (readFromKey() == 1)
+            goto loop;
+    }
+    else if (std::cin >> cmd && cmd == "/back")
+    {
+        return 0;
+    }
+loop2:
+    std::cout << "choose where to save:\n";
+    std::cout << "1.new file\n";
+    std::cout << "2.old file\n";
+
+    std::cin >> choose;
+    if (choose == 1)
+    {
+        if (saveToNew() == 1)
+            goto loop2;
+    }
+    else if (choose == 2)
+    {
+        if (saveToExist() == 1)
+            goto loop2;
+    }
+    else
+    {
+        std::cin >> cmd;
+        if (cmd == "back")
+            goto loop;
+    }
     return 0;
 }
+
+// 读取int失败后清空缓冲区
